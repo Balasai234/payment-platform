94d4774ce6191ff6417652ae21846c74a58012c4
Balasai234 <balasaicheni7@gmail.com>
2026-07-24 13:53:05 +0530

    Add processPayment method to PaymentService

--- src/main/java/com/payment/payment_service/PaymentService.java
+++ src/main/java/com/payment/payment_service/PaymentService.java
@@ -4,5 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+     public String processPayment() {
+
+        return "Payment Processed Successfully";
+
+    }
+
 
 }
