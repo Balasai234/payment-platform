182295fc11fa1fb7c533620d44276c6588d54a9b
Balasai234 <balasaicheni7@gmail.com>
2026-07-24 13:59:05 +0530

    Update PaymentController.java

--- src/main/java/com/payment/payment_service/PaymentController.java
+++ src/main/java/com/payment/payment_service/PaymentController.java
@@ -1,13 +1,16 @@
 package com.payment.payment_service.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
-   @GetMapping("/payment")
-    public String payment() {
+
+    @GetMapping("/payment")
+    public String payment(){
 
         return "Payment Service is running successfully.";
 
     }
+
 }
