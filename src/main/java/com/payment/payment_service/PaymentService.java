f56525eefecca03264e276509cad9826aeb5b1aa
Balasai234 <balasaicheni7@gmail.com>
2026-07-24 13:59:28 +0530

    Add a period to payment processed message

--- src/main/java/com/payment/payment_service/PaymentService.java
+++ src/main/java/com/payment/payment_service/PaymentService.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
-     public String processPayment() {
 
-        return "Payment Processed Successfully";
+    public String processPayment(){
+
+        return "Payment Processed Successfully.";
 
     }
-
 
 }
